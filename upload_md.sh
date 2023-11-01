67ce7f37565092d6e1ca52bb4ce282db21c4f165
Rediet <rediet@mosip.io>
2023-11-01 17:54:23 +0300

    update database url

--- upload_md.sh
+++ upload_md.sh
@@ -10,7 +10,7 @@ read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
 DB_PWD=$(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
-DB_HOST='gin-pilot-db.cy3osomkikp2.ap-south-1.rds.amazonaws.com'
+DB_HOST='13.234.110.179'
 DB_PORT=5432
 XLS=mosip_master/xlsx
 
